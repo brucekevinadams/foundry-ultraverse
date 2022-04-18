ad0864ef1e7ff2ef37025b1762662a1a8c3b72e2
Bruce Adams <22304833+brucekevinadams@users.noreply.github.com>
2022-04-18 12:17:43 -0500

    Add files via upload

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const ULTRAVERSE = {};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+ ULTRAVERSE.abilities = {
+  "str": "ULTRAVERSE.AbilityStr",
+  "dex": "ULTRAVERSE.AbilityDex",
+  "con": "ULTRAVERSE.AbilityCon",
+  "int": "ULTRAVERSE.AbilityInt",
+  "wis": "ULTRAVERSE.AbilityWis",
+  "cha": "ULTRAVERSE.AbilityCha"
+};
+
+ULTRAVERSE.abilityAbbreviations = {
+  "str": "ULTRAVERSE.AbilityStrAbbr",
+  "dex": "ULTRAVERSE.AbilityDexAbbr",
+  "con": "ULTRAVERSE.AbilityConAbbr",
+  "int": "ULTRAVERSE.AbilityIntAbbr",
+  "wis": "ULTRAVERSE.AbilityWisAbbr",
+  "cha": "ULTRAVERSE.AbilityChaAbbr"
+};
